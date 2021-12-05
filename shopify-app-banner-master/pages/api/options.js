2fd46b1ac70657ee3e69748609e680622a4ca897
thisIsMars13 <jgil@village88.com>
2021-12-05 11:32:14 +0800

    yet anothersss

--- shopify-app-banner-master/pages/api/options.js
+++ shopify-app-banner-master/pages/api/options.js
@@ -2,25 +2,26 @@ import Shopify, { DataType } from '@shopify/shopify-api';
 // import Shopify, {context} from '@lib/shopify'
 
 export default async (req, res) => {
-    const params = {
-        "product": {
-            "title":"Custom product",
-            "body_html":"Custom product",
-            "variants": [{"option1":"First","price":"456.00","sku":"123"}]
-        }
-    };
-    // `${price.toFixed(2)}`
-    const session = await Shopify.Utils.loadCurrentSession(req, res);
-    const client = new Shopify.Clients.Rest(session.shop, session.accessToken);
+  Shopify.Context.update({ HOST_NAME: req.headers.host});
+  const params = {
+      "product": {
+          "title":"Custom product",
+          "body_html":"Custom product",
+          "variants": [{"option1":"First","price":"456.00","sku":"123"}]
+      }
+  };
+  // `${price.toFixed(2)}`
+  const session = await Shopify.Utils.loadCurrentSession(req, res);
+  const client = new Shopify.Clients.Rest(session.shop, session.accessToken);
 
-    const data = await client.post({
-        path: 'products',
-        data: params,
-        type: DataType.JSON,
-    });
+  const data = await client.post({
+      path: 'products',
+      data: params,
+      type: DataType.JSON,
+  });
 
-    // res.json(data)
-    res.json("Hellos")
+  // res.json(data)
+  res.json("Hellos")
 }
 export const config = {
     api: {
